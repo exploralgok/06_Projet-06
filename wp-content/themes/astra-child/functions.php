3f4cb75c8b4fb5e5914e4d3801573025be5eda78
exploralgo <admin@exploralgo.fr>
2023-11-09 14:22:24 +0100

    hook

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -28,4 +28,24 @@ function add_google_fonts() {
     wp_enqueue_style( 'add_google_fonts', 'https://fonts.googleapis.com/css2?family=Syne:wght@400;700;800&display=swap', false );
 }
     add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
- ?>
+
+
+
+function ajouter_lien_admin_menu($items) {
+    if (is_user_logged_in() && current_user_can('administrator')) {
+        $lien_admin = '<li class="menu-item"><a href="http://localhost:8888/wp-admin/"  class="menu-link">Admin</a></li>';
+        
+        // Divisez le menu en un tableau d'éléments
+        $menu_items = explode('</li>', $items);
+
+        // Insérez le lien administrateur en deuxième position
+        array_splice($menu_items, 1, 0, $lien_admin);
+
+        // Rejoignez à nouveau les éléments du menu en une chaîne
+        $items = implode('</li>', $menu_items);
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 1);
+
+?>
